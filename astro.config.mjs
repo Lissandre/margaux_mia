89e3671e059a22a70a7c6f159bca58f47e6fde22
Lissandre <lissandre.pasdeloup@hetic.net>
2023-06-11 11:53:40 +0200

    Add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
+  compressHTML: true,
   experimental: {
     assets: true
   }
